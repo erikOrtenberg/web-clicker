a1c531adbf118e177fc49194994e75df84cb4cac
Erik Örtenberg <erik.ortenberg@gmail.com>
2024-07-25 20:00:32 +0200

    moved to flowbite css for svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,28 @@
 <script>
   import "../app.css";
   import logo from "$lib/assets/logo.png"
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  const slideParams = { delay: 0, duration: 500};
 </script>
 
-<nav class="bg-orange-600">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src={logo} class="h-20" alt="Logo" />
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Web Clicker</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-      </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-orange-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-orange-600">
-        <li>
-          <a href="/" class="block py-2 px-3 text-white rounded md:border-0 md:hover:text-blue-700 md:p-0" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/clickers" class="block py-2 px-3 text-white rounded md:border-0 md:hover:text-blue-700 md:p-0">Clickers</a>
-        </li>
-        <li>
-          <a href="/login" class="block py-2 px-3 text-white rounded md:border-0 md:hover:text-blue-700 md:p-0">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<Navbar class="bg-orange-500">
+  <NavBrand href="/">
+    <img src={logo} class="h-20" alt="Logo" />
+    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Web Clicker</span>
+  </NavBrand>
+  <NavHamburger  />
+  <NavUl slideParams={slideParams}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/clickers">Clickers</NavLi>
+    <NavLi href="/login">Login</NavLi>
+  </NavUl>
+</Navbar>
 
 <slot />
 
-
-<div class="bg-orange-600 h-20 w-full fixed bottom-0">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <div class="flex flex-row rounded px-3 py-2 bg-gray-600">
-      <a class="text-white self-center">Made with ❤️ </a>
-    </div>
+<div class="flex flex-row h-20 w-full fixed bottom-0 bg-orange-500 justify-center justify-around content-center items-center">
+  <div class="rounded px-3 py-2 bg-gray-600">
+    <a class="text-white">Made with ❤️ </a>
+    <a href="https://www.github.com" class="text-white"> Github Link</a>
   </div>
 </div>
