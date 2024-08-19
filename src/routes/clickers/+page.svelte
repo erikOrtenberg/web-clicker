bb1daf827eaa09e34c9c8b161c73e5ce4bb11acb
Erik Örtenberg <erik.ortenberg@gmail.com>
2024-08-20 00:18:57 +0200

    improved UX and design on clicker page

--- src/routes/clickers/+page.svelte
+++ src/routes/clickers/+page.svelte
@@ -1,32 +1,74 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte';
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
 	import type { PageData } from './$types';
 	
 	export let data: PageData;
+
+  export async function inc(clickerId: number, index: number){
+    const formData = new FormData();
+      formData.append("clickerId", String(clickerId))
+    const response = await fetch("/clickers?/inc", {
+        method: "POST",
+        body: formData
+      })
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    data.clickers[index].clickers.count!++;
+  }
+
+  export async function dec(clickerId: number, index: number){
+    const formData = new FormData();
+      formData.append("clickerId", String(clickerId))
+    const response = await fetch("/clickers?/dec", {
+        method: "POST",
+        body: formData
+      })
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    data.clickers[index].clickers.count!--;
+  }
 </script>
 
 <div class="flex flex-col justify-center items-center overflow-auto gap-4">
-  {#each data.clickers as clicker}
-      <div class="flex flex-row  min-w-60 max-w-60 min-h-32 m-4">
-        <div class="basis-1/4 bg-red-500 hover:bg-red-700 rounded-l-lg">
-          <form method="POST" class="h-full w-full" action="?/dec">
-            <button value={clicker.clickers.id} name="clickerId" type="submit" class="h-full w-full">-</button>
-          </form>
+  {#each data.clickers as clicker, i}
+      <div class="flex flex-row rounded-lg outline outline-primary-600 min-w-60 max-w-60 min-h-32 m-4">
+        <div class="basis-1/4 rounded-l-lg">
+          <button class="h-full w-full" on:mouseup={() => dec(clicker.clickers.id, i)}>
+            <p class="text-4xl">-</p>
+          </button>
         </div>
-        <div class="basis-2/4 from-green-500 to-red-500 flex justify-center items-center p-4">
+        <div class="basis-2/4 shadow-inner bg-gray-200 outline outline-primary-600 flex justify-center items-center p-4">
           <div class="flex flex-col items-center justify-center">
             <p class="basis-1/4 text-lg dark:text-white">{clicker.items.name}</p>
             <p class="basis-3/4 text-4xl dark:text-white">{clicker.clickers.count}</p>
           </div>
         </div>
-        <div class="basis-1/4 bg-green-500 hover:bg-green-700 rounded-r-lg">
-          <form method="POST" class="h-full w-full" action="?/inc">
-            <button value={clicker.clickers.id} name="clickerId" type="submit" class="h-full w-full">+</button>
-          </form>
+        <div class="basis-1/4 rounded-r-lg">
+          <button class="h-full w-full" on:mouseup={() => inc(clicker.clickers.id, i)}>
+            <p class="text-4xl">+</p>
+          </button>
         </div>
       </div>
-        <p>note: {clicker.items.comment} </p>
-        <p>price: {clicker.items.price} kr</p>
   {/each}
+  <div class="rounded-lg outline outline-primary-600 bg-gray-200 m-4">
+    <Table color="custom">
+      <TableHead>
+        <TableHeadCell class="text-black">Item</TableHeadCell>
+        <TableHeadCell class="text-black">Price</TableHeadCell>
+        <TableHeadCell class="text-black">Comment</TableHeadCell>
+      </TableHead>
+      {#each data.clickers as clicker}
+      <TableBody>
+        <TableBodyRow>
+          <TableBodyCell class="text-black">{clicker.items.name}</TableBodyCell>
+          <TableBodyCell class="text-black">{clicker.items.price} kr</TableBodyCell>
+          <TableBodyCell class="text-black">{clicker.items.comment}</TableBodyCell>
+        </TableBodyRow>
+      </TableBody>
+      {/each}
+    </Table>  
+  </div>
 </div>
 
