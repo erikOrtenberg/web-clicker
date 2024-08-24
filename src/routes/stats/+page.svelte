016a619b5dc6d67de4b665fa88bbedab50a7fed6
Erik Örtenberg <erik.ortenberg@gmail.com>
2024-08-25 01:19:57 +0200

    fixed rounded edges in stats page

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -6,8 +6,8 @@
 
 <div class="flex flex-col justify-center items-center overflow-auto gap-4">
   {#each data.spendings as spend}
-      <div class="flex flex-row rounded-lg outline justify-center items-center outline-primary-600 min-w-60 max-w-60 min-h-32 m-4">
-        <div class="flex flex-col shadow-inner bg-gray-200 justify-center items-center w-full h-full p-4">
+      <div class="flex flex-row rounded-lg outline justify-center items-center shadow-inner bg-gray-200 outline-primary-600 min-w-60 max-w-60 min-h-32 m-4">
+        <div class="flex flex-col justify-center items-center w-full h-full p-4">
             <p class="basis-1/4 text-lg dark:text-white">{spend.name}</p>
             <p class="basis-1/4 text-lg dark:text-white">Count: {spend.count}</p>
             <p class="basis-2/4 text-4xl dark:text-white">{spend.total} kr</p>
