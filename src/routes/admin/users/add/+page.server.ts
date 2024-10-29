f3598597b577f0340a5c63d58caa6aee070abc25
Erik Örtenberg <erik.ortenberg@gmail.com>
2024-10-29 16:07:14 +0100

    made user creation create all available clickers for them

--- src/routes/admin/users/add/+page.server.ts
+++ src/routes/admin/users/add/+page.server.ts
@@ -1,6 +1,8 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
-import { createUser } from "../../../../lib/server/api/users";
+import { createUser } from "$lib/server/api/users";
+import { getItems } from "$lib/server/api/items";
+import { createClickerForUser } from "$lib/server/api/clickers";
 
 export const actions = {
   default: async ({ request, locals }) => {
@@ -16,6 +18,7 @@ export const actions = {
         message: "Please provide a complete user. " + request.json(),
       });
     }
+    //data good, Create user
     const result = await createUser(String(name), String(password));
 
     if (!result) {
@@ -23,6 +26,13 @@ export const actions = {
         message: "Couldn't create user with params. " + request.json(),
       });
     }
+    // we have user, create all available clickers for them
+    const items = await getItems();
+    items.forEach(item => {
+      createClickerForUser(result.lastInsertRowid, item.id);
+    })
+    
+
     redirect(303, "/admin/users");
   },
 } satisfies Actions;
