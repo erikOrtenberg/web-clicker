f85b3f7d4e9a12a447d0f7aecb1574c81e71cf61
Erik Örtenberg <erik.ortenberg@gmail.com>
2025-02-25 12:41:16 +0100

    added support to add a custom transaction to a clicker for admin

--- src/routes/admin/clickers/update/[slug]/+page.server.ts
+++ src/routes/admin/clickers/update/[slug]/+page.server.ts
@@ -2,6 +2,7 @@ import { error, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 import type { PageServerLoad } from "./$types";
 import { getClickerById, updateClickerForUser } from "$lib/server/api/clickers";
+import { createTransactionForClicker } from "$lib/server/api/transactions";
 
 export const load: PageServerLoad = async ({ params }) => {
   const clickerId = parseInt(params.slug);
@@ -34,14 +35,32 @@ export const actions = {
     }
     const data = await request.formData();
     const clickNumber = data.get("click_number");
+    const customTransactionCount = data.get("custom_transaction_count");
+    var parsedTransactionCount = 0
     if (!clickNumber) {
       error(400, "you must provide a count");
+    }
+    if (!customTransactionCount) {
+    } else if (isNaN(parseInt(String(customTransactionCount)))){
+      error(400, "you must provide an integer")
+    }else {
+      parsedTransactionCount = Number(customTransactionCount)
     }
     if (isNaN(parseInt(String(clickNumber)))) {
       error(400, "you must provide an integer");
     }
 
     const clicker = await getClickerById(clickerId);
+
+    if(parsedTransactionCount != 0){
+      const resultTransaction = await createTransactionForClicker(clickerId, parsedTransactionCount);
+
+      if (!resultTransaction) {
+        error(404, {
+          message: "Couldn't create transaction with params. ",
+        });
+      }
+    }
 
     const result = await updateClickerForUser(
       clickerId,
--- src/routes/admin/clickers/update/[slug]/+page.svelte
+++ src/routes/admin/clickers/update/[slug]/+page.svelte
@@ -6,9 +6,15 @@
 
 <form method="POST">
   <div class="flex flex-row items-center justify-center p-4">
-    <div class="mb-6 p-4 min-w-52">
-      <Label for="click_number-input" class="block mb-2">Default Click Value</Label>
-      <Input name="click_number" value={data.clicker.clickers.click_number} id="count-input" type="number" placeholder={data.clicker.clickers.click_number} />
+    <div class="flex flex-row items-center justify-center mb-6 p-4 min-w-96">
+      <div class="p-4">
+        <Label for="click_number-input" class="block mb-2">Default Click Value</Label>
+        <Input name="click_number" value={data.clicker.clickers.click_number} id="count-input" type="number" placeholder={data.clicker.clickers.click_number} />
+      </div>
+      <div class="p-4">
+        <Label for="click_number-input" class="block mb-2">Add Transaction</Label>
+        <Input name="custom_transaction_count" value=0 id="count-input" type="number" placeholder=0 />
+      </div>
     </div>
     <Button outline color="green" class="bg-white" type="submit">Update Clicker</Button>
   </div>
